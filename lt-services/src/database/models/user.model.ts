a15e56cb617a5bc02293f58fe9d6d6c69daa3fcc
Dimakatso <dimakatsomm@icloud.com>
2024-03-11 23:23:36 +0200

    Hash password before saving

--- lt-services/src/database/models/user.model.ts
+++ lt-services/src/database/models/user.model.ts
@@ -1,6 +1,8 @@
 import { randomUUID } from "crypto";
 import { Schema, model } from "mongoose";
 import { IUser } from "database/types/user.type";
+import { genSalt, hash } from "bcrypt";
+import * as C from "../../constants";
 
 const userSchema = new Schema({
     _id: {
@@ -19,12 +21,34 @@ const userSchema = new Schema({
     },
     emailAddress: {
         type: String,
-        required: true
+        required: true,
+        unique: true,
+        
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
     },
     password: {
         type: String,
         required: true,
     },
 }, { timestamps: true });
+
+userSchema.pre('save', async function (next) {
+    const user = this;
+
+    if (!user.isModified('password')) return next();
+
+    try {
+        const salt = await genSalt(C.SALT_WORK_FACTOR);
+    user.password = await hash(user.password, salt);
+    next();
+
+    } catch(e: any) {
+        next(e);
+    }
+})
 
 export const User = model<IUser, any>('User', userSchema, 'users');
