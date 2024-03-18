280b7bc76256d7a7586c9fb598c10bfdb60c2ba0
Dimakatso <dimakatsomm@icloud.com>
2024-03-18 15:16:36 +0200

    Add Country Data Seeding

--- lt-services/src/database/seeders/country.seeder.ts
+++ lt-services/src/database/seeders/country.seeder.ts
@@ -1,1 +1,34 @@
-// TODO: Add seeder
+import mongoose from 'mongoose';
+
+import * as C from '../../constants';
+import { Country } from '../models/country.model';
+import countries from './countries.json';
+import { exit } from 'process';
+
+const resolvedCountries = countries.map(c => ({
+    _id: c.code,
+    name: c.name,
+    capital: c.capital,
+    region: c.region,
+    language: c.language,
+    currency: c.currency,
+    flag: c.flag
+}));
+
+(async () => {
+    try {
+        await mongoose.connect(C.MONGOURI);
+        const country = await Country.findOne({ _id: countries[0].code });
+        console.log(country);
+        if (!country) {
+            await Country.insertMany(resolvedCountries);
+        } else {
+            console.log('Countries already exist.');
+        }
+
+        exit();
+    } catch (e) {
+        console.error(e);
+        exit(1);
+    }
+})();
